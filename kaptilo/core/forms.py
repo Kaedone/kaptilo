44e6b5028b93d964d75770fdfb5fd76d3595d7be
Pashs-ba <pashs.ba@gmail.com>
2021-03-20 15:53:47 +0300

    make link show

--- kaptilo/core/forms.py
+++ kaptilo/core/forms.py
@@ -4,8 +4,8 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class LinkForm(forms.Form):
-    text = forms.CharField(label="Text in note", widget=forms.Textarea)
-    # url = forms.SlugField(label="Link name")
+    text = forms.CharField(label="Text in note", widget=forms.Textarea, required=False)
+    header = forms.CharField(label="Header", required=True)
     is_delete = forms.BooleanField(label='Delete after reading', required=False)
 
 
--- kaptilo/core/urls.py
+++ kaptilo/core/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('create-link', views.create_link, name="create_link"),
     path('login/', views.login, name="login"),
     # path('auth/', views.auth, name='auth')
-    path('logout/', views.logout, name="logout")
+    path('logout/', views.logout, name="logout"),
+    path('<int:id>', views.show_link, name='show_link')
 ]
