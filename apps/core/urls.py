f11181d4213760f055ba0b63278e399f695ecc64
DrMartiner <DrMartiner@GMail.Com>
2021-03-20 22:56:33 +0200

    Fixed imports

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import views
+from apps.core import views
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
--- apps/core/views.py
+++ apps/core/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.db import transaction
 from django.shortcuts import render, redirect
 
-from core import forms
-from . import models
+from apps.core import forms
+from apps.core import models
 
 
 def homepage(request):
