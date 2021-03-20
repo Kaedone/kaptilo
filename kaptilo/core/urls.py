b85eac76b9523ea0d124f6c0f53bcb762a3cdab2
Pashs-ba <pashs.ba@gmail.com>
2021-03-20 13:57:22 +0300

    make form

--- kaptilo/core/urls.py
+++ kaptilo/core/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.homepage, name="homepage")
+    path('', views.homepage, name="homepage"),
+    path('create-link', views.create_link, name="create_link")
+
 ]
--- kaptilo/core/views.py
+++ kaptilo/core/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .forms import *
 
 # Create your views here.
 
 
 def homepage(request):
     return render(request, 'homepage.html', {})
+
+
+def create_link(request):
+    content = {}
+    if request.method == 'POST':
+        pass
+    else:
+        content = {'form': LinkForm()}
+    return render(request, 'create_link.html', content)
